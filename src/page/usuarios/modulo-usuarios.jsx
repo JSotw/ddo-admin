7a6730614f5c1390192e1bc316909cf2e261ac4b
JSotw <janoguerrasks@gmail.com>
2024-05-13 20:51:20 -0400

    .

--- src/page/usuarios/modulo-usuarios.jsx
+++ src/page/usuarios/modulo-usuarios.jsx
@@ -95,76 +95,78 @@ const ModuloUsuarios = () => {
               </tr>
             </thead>
             <tbody>
-              {usuarios?.map((u) => {
-                if (u.rol != "Administrador") {
-                  <tr key={u._id}>
-                    <td className="p-2 align-middle h-20 bg-transparent border-b whitespace-nowrap shadow-transparent">
-                      <div className="flex px-2 py-1 justify-center">
-                        <div>
-                          <FaUser />{" "}
+              {usuarios?.map((u) => (
+                <>
+                  {u.rol !== "Administrador" ? (
+                    <tr key={u._id}>
+                      <td className="p-2 align-middle h-20 bg-transparent border-b whitespace-nowrap shadow-transparent">
+                        <div className="flex px-2 py-1 justify-center">
+                          <div>
+                            <FaUser />{" "}
+                          </div>
+                          <div className="flex flex-col justify-center">
+                            <p className="mb-0 pl-2 font-semibold leading-tight text-xs">
+                              {`${u.nombre_usuario}`}
+                            </p>
+                          </div>
                         </div>
-                        <div className="flex flex-col justify-center">
-                          <p className="mb-0 pl-2 font-semibold leading-tight text-xs">
-                            {`${u.nombre_usuario}`}
-                          </p>
-                        </div>
-                      </div>
-                    </td>
-                    <td className="p-2 align-middle bg-transparent border-b w-auto h-auto whitespace-nowrap shadow-transparent">
-                      <p className="mb-0 leading-tight text-xs text-slate-400 truncate text-balance text-ellipsis overflow-hidden">
-                        {`${u.primer_n} ${u.apellido_p}`}
-                      </p>
-                    </td>
-                    <td className="p-2 align-middle bg-transparent border-b w-auto h-auto whitespace-nowrap shadow-transparent">
-                      <p className="mb-0 leading-tight text-xs text-slate-400 truncate text-balance text-ellipsis overflow-hidden">
-                        {`${u.rol}`}
-                      </p>
-                    </td>
-                    <td
-                      className="p-2 leading-normal text-center align-middle bg-transparent border-b text-sm 
-                    whitespace-nowrap shadow-transparent"
-                    >
-                      <span
-                        className={`${
-                          u.activo ? `bg-green-400` : "bg-red-400"
-                        } px-3.6 text-xs rounded 
-                      py-2.2 inline-block whitespace-nowrap text-center align-baseline font-normal p-1
-                        leading-none text-white `}
+                      </td>
+                      <td className="p-2 align-middle bg-transparent border-b w-auto h-auto whitespace-nowrap shadow-transparent">
+                        <p className="mb-0 leading-tight text-xs text-slate-400 truncate text-balance text-ellipsis overflow-hidden">
+                          {`${u.primer_n} ${u.apellido_p}`}
+                        </p>
+                      </td>
+                      <td className="p-2 align-middle bg-transparent border-b w-auto h-auto whitespace-nowrap shadow-transparent">
+                        <p className="mb-0 leading-tight text-xs text-slate-400 truncate text-balance text-ellipsis overflow-hidden">
+                          {`${u.rol}`}
+                        </p>
+                      </td>
+                      <td
+                        className="p-2 leading-normal text-center align-middle bg-transparent border-b text-sm 
+                whitespace-nowrap shadow-transparent"
                       >
-                        {`${u.activo ? `Activo` : "Inactivo"}`}
-                      </span>
-                    </td>
-                    <td className="p-2 text-center align-middle bg-transparent border-b whitespace-nowrap shadow-transparent">
-                      <span className="font-semibold leading-tight text-xs text-slate-400">
-                        {u.correo}
-                      </span>
-                    </td>
-                    <td
-                      className="p-2 gap-4 bg-transparent border-b
-                    flex-col h-full"
-                    >
-                      <button className="transition duration-300 hover:text-blue-400">
-                        <FaUserEdit size={14} />
-                      </button>
-                      <button
-                        onClick={() => openDelete(u._id, u.nombre_usuario)}
-                        className="transition duration-300 hover:text-red-400"
+                        <span
+                          className={`${
+                            u.activo ? `bg-green-400` : "bg-red-400"
+                          } px-3.6 text-xs rounded 
+                  py-2.2 inline-block whitespace-nowrap text-center align-baseline font-normal p-1
+                    leading-none text-white `}
+                        >
+                          {`${u.activo ? `Activo` : "Inactivo"}`}
+                        </span>
+                      </td>
+                      <td className="p-2 text-center align-middle bg-transparent border-b whitespace-nowrap shadow-transparent">
+                        <span className="font-semibold leading-tight text-xs text-slate-400">
+                          {u.correo}
+                        </span>
+                      </td>
+                      <td
+                        className="p-2 gap-4 bg-transparent border-b
+                flex-col h-full"
                       >
-                        <FaUserTimes size={14} />
-                      </button>
-                    </td>
-                    {/* <td className="p-2 align-middle bg-transparent border-b whitespace-nowrap shadow-transparent">
-                    <a
-                      href="javascript:;"
-                      className="font-semibold leading-tight text-xs text-slate-400"
-                    >
-                      {" "}
-                      Ver más{" "}
-                    </a>
-                  </td> */}
-                  </tr>;
-                }
-              })}
+                        <button className="transition duration-300 hover:text-blue-400">
+                          <FaUserEdit size={14} />
+                        </button>
+                        <button
+                          onClick={() => openDelete(u._id, u.nombre_usuario)}
+                          className="transition duration-300 hover:text-red-400"
+                        >
+                          <FaUserTimes size={14} />
+                        </button>
+                      </td>
+                      {/* <td className="p-2 align-middle bg-transparent border-b whitespace-nowrap shadow-transparent">
+                <a
+                  href="javascript:;"
+                  className="font-semibold leading-tight text-xs text-slate-400"
+                >
+                  {" "}
+                  Ver más{" "}
+                </a>
+              </td> */}
+                    </tr>
+                  ) : null}
+                </>
+              ))}
             </tbody>
           </table>
         </section>
